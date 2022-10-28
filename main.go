f3bd56c427efeeaea8352607fa54bd07cfae7a3c
xiaolatiao <1628652790@qq.com>
2022-10-29 00:14:30 +0800

    feat: 完成权限路由表初始化

--- main.go
+++ main.go
@@ -7,13 +7,16 @@ import (
 )
 
 func main() {
-	r := gin.New()
+	r := gin.Default()
 	r.Use(lib.Middlewares()...)
 	r.GET("/depts", func(context *gin.Context) {
 		context.JSON(200, gin.H{"result": "部门列表"})
 	})
 	r.POST("/depts", func(context *gin.Context) {
 		context.JSON(200, gin.H{"result": "批量修改部门列表"})
+	})
+	r.GET("/depts/:id", func(ctx *gin.Context) {
+		ctx.JSON(200, gin.H{"result": "详情"})
 	})
 	r.Run(":8080")
 
